6c37e32af5fe3ccc362eac388f3ffbc00ad1365c
soohyeon <qpsxhf410@outlook.kr>
2023-10-11 23:22:38 +0900

    레벨 0

---
+++ Lv.0/세로 읽기.py
@@ -0,0 +1,5 @@
+def solution(my_string, m, c):
+    answer = ''
+    tmp = [my_string[i:i+m] for i in range(0, len(my_string), m)]
+    answer = "".join([tmp[i][c-1] for i in range(0, len(tmp))])
+    return answer
