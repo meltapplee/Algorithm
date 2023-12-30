64939cfec75d7f32846096ae9154f7570a213780
soohyeon <qpsxhf410@outlook.kr>
2023-12-30 23:25:16 +0900

    레벨 1

---
+++ Lv.1/K번째수.py
@@ -0,0 +1,9 @@
+def solution(array, commands):
+    answer = []
+    result = []
+    for command in commands:
+        result = array[command[0]-1:command[1]]
+        result.sort()
+        answer.append(result[command[2]-1])
+        
+    return answer
